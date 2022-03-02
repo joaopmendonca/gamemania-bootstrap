d9379ce2c41484d6d207c1026d8dd178d9e96cc7
João Paulo <82663879+joaopmendonca@users.noreply.github.com>
2022-03-02 17:04:56 -0300

    Footer
    Campo de cadastro de e-mail inserido

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let email = document.getElementById("campo-email"); //busca no html a id "campo-email" e salva na variavel email
+
+function enviarEmail() {
+    let emailDigitado = email.value;
+    console.log(email);
+}
+
+// function mostrarPopup() {
+//     window.alert("Seja bem vindo!");
+// }
